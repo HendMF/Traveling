a2e5479856f23df622397b151141cd31fc328632
Hind Mahmoud <hindmahmoudfouad93@gmail.com>
2023-06-03 19:00:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./component/homePage/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <HomePage/>
+        <Routes>
+          <Route />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
